05194b61c1175d5b9d90f32652d5522c5d1d86c7
leakira <leakira.uac@gmail.com>
2018-11-16 13:08:06 -0200

    Adds remote install bash script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+################
+### Script for remote install
+################
+
+curl -o goto-master.zip https://codeload.github.com/leakira/goto/zip/master
+unzip goto-master.zip
+cd goto-master && ./install.py
+cd ..
+rm goto-master.zip
+rm -rf goto-master
